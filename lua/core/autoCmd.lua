ca62fa1bfa5cb7a028abeb26d7e5e2376fbc0f77
Sveet-Pickle <damitchell89@protonmail.com>
2023-11-15 19:43:36 -0500

    updated autocommand to set textwrapping in markdown files

--- lua/core/autoCmd.lua
+++ lua/core/autoCmd.lua
@@ -2,25 +2,24 @@ vim.api.nvim_create_augroup("quicknote", { clear = true })
 vim.api.nvim_create_augroup("markdown", { clear = true })
 
 vim.api.nvim_create_autocmd("bufenter", {
-    pattern = "quicknote.md",
-    callback = function()
-        vim.schedule(function()
-            print("quicknote opened")
-        end)
-        vim.cmd(":%d")
-    end,
-    group = "quicknote",
-}
-)
+	pattern = "quicknote.md",
+	callback = function()
+		vim.schedule(function()
+			print("quicknote opened")
+		end)
+		vim.cmd(":%d")
+	end,
+	group = "quicknote",
+})
 
 vim.api.nvim_create_autocmd("bufenter", {
-    pattern = "*.md",
-    callback = function()
-        vim.schedule(function()
-            print("markdown file opened")
-        end)
-        vim.cmd(":set noexpandtab")
-    end,
-    group = "markdown",
-}
-)
+	pattern = "*.md",
+	callback = function()
+		vim.schedule(function()
+			print("markdown file opened")
+		end)
+		vim.cmd(":set noexpandtab")
+		vim.cmd(":set textwidth=80")
+	end,
+	group = "markdown",
+})
